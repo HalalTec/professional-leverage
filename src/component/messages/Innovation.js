2a5fc8eeb37ed99cbadf2521c4c2d07c85455c33
HalalTec <134257011+HalalTec@users.noreply.github.com>
2025-04-07 11:07:35 +0100

    Update Innovation.js

--- src/component/messages/Innovation.js
+++ src/component/messages/Innovation.js
@@ -224,17 +224,6 @@ const Innovation = ({close, msg, submit, quest}) => {
                 'Can I sustain my leadership position in innovation and agility?',
                 'How do I continuously refine my methods to stay ahead?'
             ]
-        },
-        {
-            score: 10,
-            indication: 'Exceptional spiritual connection, profound inner peace, complete alignment with purpose, deeply fulfilling spiritual practices, strong sense of transcendence.',
-            reflectiveQuestions: [
-                'Do you feel a profound, unbreakable connection to your spirituality and purpose?',
-                'Are you consistently at peace, with a deep sense of fulfillment and clarity?',
-                'Does your spirituality define and elevate your daily life?'
-
-
-            ]
         }
 
       ];
